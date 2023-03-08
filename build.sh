fd6d4195f0a65424b1015fcbcbd05503dc7c0a91
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2023-03-08 11:53:49 -0800

    🔧 Update Build Script

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ SFML_PATH=/opt/homebrew/Cellar/sfml/2.5.1_2
 # Get all cpp files in the src directory
 cFilenames=$(find ./src -type f -name "*.cpp")
 
-g++ $cFilenames -I$SFML_PATH/include -o bin/app -L$SFML_PATH/lib -lsfml-graphics -lsfml-window -lsfml-system
+g++ $cFilenames -I$SFML_PATH/include -o bin/app -L$SFML_PATH/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
